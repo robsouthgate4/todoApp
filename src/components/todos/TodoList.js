e2c2bbbbf243c073a9a136faa0bf2233228a373c
Robert Southgate <Robert Southgate>
2017-02-19 23:11:29 +0000

    Played with transitions

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,11 +1,23 @@
 import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { TodoItem } from './TodoItem';
 
-export const TodoList = (props) => (
+export const TodoList = (props) => {
+  const mappedTodos =  props.todos.map((todo, index) => <TodoItem handleDelete={props.handleDelete} handleToggle={props.handleToggle} key={index} {...todo}></TodoItem>); 
+  return (
     <div className="todo-list">
       <ul>
-        { props.todos.map((todo, index) => <TodoItem handleDelete={props.handleDelete} handleToggle={props.handleToggle} key={index} {...todo}></TodoItem>) }
+        <ReactCSSTransitionGroup
+          key={1}
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {mappedTodos}
+        </ReactCSSTransitionGroup>
       </ul>
-    </div>)
+    </div>
+  )
+
+  }
 
 // TodoList.PropTypes
